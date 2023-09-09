f9e562912afd8a50e3049d5bcd3e3ab48ed6c2c6
hamzarahmoun <hamza.rahmoun456@gmail.com>
2023-09-09 09:28:19 +0000

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,7 +8,7 @@ import { useInView, useMotionValue, useSpring } from 'framer-motion'
 import { Skills } from '../components/Skills'
 import { Experience } from '../components/Experience'
 import { Education } from '../components/Education'
-import { TransitionEffect } from '/components/TransitionEffect'
+import { TransitionEffect } from '../components/TransitionEffect'
 
 const AnimatedNumbers = ({ value }) => {
     const ref = useRef(null)
