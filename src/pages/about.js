d4b0dcee5325557906eecd74844a1aaec23605eb
hamzarahmoun <hamza.rahmoun456@gmail.com>
2023-09-09 09:25:54 +0000

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,13 +1,13 @@
-import { AnimatedText } from '@/components/AnimatedText'
-import { Layout } from '@/components/Layout'
+import { AnimatedText } from '../components/AnimatedText'
+import { Layout } from '../components/Layout'
 import Head from 'next/head'
 import React, { useEffect, useRef } from 'react'
 import Image from 'next/image'
 import profilePic from "../../public/images/profile/back.jpg"
 import { useInView, useMotionValue, useSpring } from 'framer-motion'
-import { Skills } from '@/components/Skills'
-import { Experience } from '@/components/Experience'
-import { Education } from '@/components/Education'
+import { Skills } from '../components/Skills'
+import { Experience } from '../components/Experience'
+import { Education } from '../components/Education'
 import { TransitionEffect } from '@/components/TransitionEffect'
 
 const AnimatedNumbers = ({ value }) => {
